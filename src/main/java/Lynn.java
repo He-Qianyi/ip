58378499eaefd1e249b27d911a507da8a32c0be9
He-Qianyi <e1513117@u.nus.edu>
2026-08-20 16:52:07 +0800

    Add greeting and exit flow for Level-0

--- src/main/java/Lynn.java
+++ src/main/java/Lynn.java
@@ -6,6 +6,14 @@ public class Lynn {
                 + "| |___| |_| | | | | | | |  \n"
                 + "|_____|\\__, |_| |_|_| |_|  \n"
                 + "       |___/               \n";
+        String line = "____________________________________________________________";
+
+        System.out.println(line);
         System.out.println(banner);
+        System.out.println("Hello! I'm Lynn.");
+        System.out.println("How can I help you today?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
